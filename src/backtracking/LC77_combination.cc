ce5caf20167d37fa9392220e96496204866dfcf2
yuzi <yuzi_csu@163.com>
2024-07-16 22:13:52 +0800

    LC77

--- src/backtracking/LC77_combination.cc
+++ src/backtracking/LC77_combination.cc
@@ -1,10 +1,38 @@
 #include <iostream>
-
+#include <vector>
 
 using namespace std;
 
-int main(){
-    cout << "Hello world" << endl;
-    return 0;
+vector<vector<int>> res;
+vector<int> path;
+
+void back_tracking(int n, int k, int start_index) {
+    if (path.size() == k) {
+        res.push_back(path);
+        return;
+    }
+
+    if ((n - start_index + 1) < k) { //¼ôÖ¦ÓÅ»¯
+        return;
+    }
+
+    for(int i = start_index; i <= n; i++) {
+        path.push_back(i);
+        back_tracking(n, k, i + 1);
+        path.pop_back();
+    }
+
+}
+
+int  main() {
+    
+    back_tracking(5, 2, 1);
+    
+    for (int i = 0; i < res.size(); i++){
+        for (int j = 0; j < (res[i]).size(); j++) {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
